6f750412dd22c35edee6eb43714e90bbd6fd29bd
Arthur Pedroti <arthurpedroti@gmail.com>
2019-11-21 17:29:00 -0300

    OU and EMP controllers

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -4,9 +4,13 @@ const routes = express.Router();
 const EstoqueController = require('./controllers/EstoqueController');
 const PCsController = require('./controllers/PCsController');
 const SCsController = require('./controllers/SCsController');
+const EmpController = require('./controllers/EmpController');
+const OuController = require('./controllers/OuController');
 
 routes.get('/estoques', EstoqueController.index);
 routes.get('/pcs', PCsController.index);
 routes.get('/scs', SCsController.index);
+routes.get('/emp', EmpController.index);
+routes.get('/ou', OuController.index);
 
 module.exports = routes;
